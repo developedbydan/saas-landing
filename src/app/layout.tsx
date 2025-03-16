5f24ed2d2d8501e39bafd8ee804a4a1e877be174
Danijel <developedbydane@gmail.com>
2025-03-16 19:06:50 +0100

    Added hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
+        data-theme="dark"
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
---
+++ src/components/logo.tsx
@@ -0,0 +1,9 @@
+import { cn } from "@/lib/utils";
+
+export const Logo = ({ className }: { className?: string }) => {
+  return (
+    <div>
+      <h3 className="text-2xl font-bold">CodeFlow</h3>
+    </div>
+  );
+};
